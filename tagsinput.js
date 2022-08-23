029b87f42f17a22838a72c678ea187b0214311ac
Walaa Alshaikh <walaalshaikh94@gmail.com>
2022-08-23 12:36:09 +0300

    adding pictures

---
+++ tagsinput.js
@@ -0,0 +1,37 @@
+// $('input').tagsinput('add', { "value": 1 , "text": "jQuery"});
+
+// $('input').tagsinput('add', { "value": 2, "text": "Script"});
+
+// $('input').tagsinput('add', { "value": 3, "text": "Net"});
+
+// $("input").val()
+// $("input").tagsinput('items')
+// {
+//     tagClass: function(item) {
+//       return 'badge badge-info';
+//     },
+//     focusClass: 'focus',
+//     itemValue: function(item) {
+//       return item ? item.toString() : item;
+//     },
+//     itemText: function(item) {
+//       return this.itemValue(item);
+//     },
+//     itemTitle: function(item) {
+//       return null;
+//     },
+//     freeInput: true,
+//     addOnBlur: true,
+//     maxTags: undefined,
+//     // maxChars: undefined,
+//     confirmKeys: [13, 44],
+//     delimiter: ',',
+//     delimiterRegex: null,
+//     cancelConfirmKeysOnEmpty: false,
+//     onTagExists: function(item, $tag) {
+//       $tag.hide().fadeIn();
+//     },
+//     trimValue: false,
+//     allowDuplicates: false,
+//     triggerChange: true
+//   }
